812904aaf138482a05d2e0511aa3f53c769992a1
Angelini <angelini75@gmail.com>
2020-03-28 10:58:01 +0100

    reorganizacion de tabla de atributos de mapa 1:50:000

--- code/BDS50.R
+++ code/BDS50.R
@@ -1,4 +1,4 @@
-# Estimación de la superficie no agricola de suelo del norte de la provincia de Buenos Aires
+# Organización de la tabla de atributos del mapa 1:50000 de Buenos Aires
 
 library(tidyverse)
 library(sf)
@@ -34,14 +34,33 @@ ucar_c <- s %>%
   unique() %>% 
   select(-PAISAJE)
 
-ucar_c <- ucar_c %>% 
-  pivot_longer(
-    cols = SERIE1:POSIC6,
-    names_to = c("serie", "porc", "fase", "posicion"), 
-    names_pattern = "new_?(.*)_(.)(.*)",
-    values_to = "count"
-  )
+u <- NULL
+for (i in as.character(1:5)) {
+  x <- ucar_c[,c(1,grep(pattern = i, x = names(ucar_c)))]  
+  names(x) <- c('ucar_id', 'serie', 'porc', 'fase', 'posicion')
+  u <- rbind(u,x)
+}
 
+ucar_c <- u
+  
+hoja <- ucar_d %>% 
+  select(hoja_ign, nombre, simb, ucar_id, poli_id) %>% 
+  unique()
 
-  select(SERIE1:taxo_3rio) %>% 
-  pivot_longer(-poli_id)
+poligonos <- ucar_d %>% 
+  select(poli_id, ucar_id, simb) %>% 
+  unique()
+
+ucar_d <- ucar_d %>% 
+  select(-hoja_ign, -nombre, -poli_id) %>% 
+  unique() 
+
+ucar <- ucar_d %>% 
+  group_by(simb) %>% 
+  summarise(n_simb = n()) %>% 
+  left_join(ucar_d) 
+
+write_csv(hoja, 'data/ucar_x_hoja_ign.csv')
+write_csv(poligonos, 'data/ucar_x_poligon.csv')
+write_csv(ucar, 'data/ucar_atributos.csv')
+write_csv(ucar_c, 'data/ucar_composicion.csv')
