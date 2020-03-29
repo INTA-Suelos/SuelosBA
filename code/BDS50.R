81aa24e22858901d13c7671cdfd0a4ea02d433bf
Angelini <angelini75@gmail.com>
2020-03-29 13:35:04 +0200

    Nueva tabla de series/nombre de componente

--- code/BDS50.R
+++ code/BDS50.R
@@ -26,22 +26,32 @@ s <- s %>%
 
 # caracteristicas de las UCAR
 ucar_d <- s %>% 
-  select(poli_id:tipo, cap_uso:taxo_3rio)
+  select(poli_id:tipo, cap_uso:act)
 
 # Composicion de las UCAR
 ucar_c <- s %>% 
-  select(ucar_id, simb, SERIE1:POSIC6) %>% 
+  select(ucar_id, simb, SERIE1:POSIC6, taxo_ppal:taxo_3rio) %>% 
   unique() %>% 
   select(-PAISAJE)
 
+names(ucar_c)[27:29] <- c('taxo_1', 'taxo_2', 'taxo_3')
+
 u <- NULL
-for (i in as.character(1:5)) {
+for (i in as.character(1:3)) {
   x <- ucar_c[,c(1, 2,grep(pattern = i, x = names(ucar_c)))]  
-  names(x) <- c('ucar_id', 'simb', 'serie', 'porc', 'fase', 'posicion')
+  names(x) <- c('ucar_id', 'simb', 'serie', 'porc', 'fase', 'posicion', 'taxo')
   u <- rbind(u,x)
 }
+u2 <- NULL
+for (i in as.character(4:5)) {
+  x <- ucar_c[,c(1, 2,grep(pattern = i, x = names(ucar_c)))]  
+  x$taxo <- NA
+  names(x) <- c('ucar_id', 'simb', 'serie', 'porc', 'fase', 'posicion', 'taxo')
+  u2 <- rbind(u,x)
+}
 
-ucar_c <- u
+
+ucar_c <- rbind(u,u2)
   
 hoja <- ucar_d %>% 
   select(hoja_ign, nombre, simb, ucar_id, poli_id) %>% 
@@ -55,10 +65,14 @@ ucar_d <- ucar_d %>%
   select(-hoja_ign, -nombre, -poli_id) %>% 
   unique() 
 
+ucar_d$tipo[ucar_d$tipo== 'falta'] <- NA
+ucar_d$cap_uso[ucar_d$cap_uso== 'falta'] <- NA
+ucar_d$descr_ucar <- gsub(pattern = 'falta ', replacement = '', x = ucar_d$descr_ucar)
 ucar <- ucar_d %>% 
   group_by(simb) %>% 
   summarise(n_simb = n()) %>% 
   left_join(ucar_d) 
+
 ucar
 
 ucar_c <- ucar_c %>% 
@@ -100,9 +114,16 @@ ucar_porc_error <- ucar_c %>%
   summarise(sum_porc = sum(porc)) %>% 
   filter(sum_porc != 100)
 write_csv(ucar_porc_error, 'data/_ucar_porc_error.csv')
-#
 
+# Conectar componentes ucar_c con taxonomía de ucar_d
+serie <- ucar_c %>% 
+  select(serie, porc, ucar_id, simb) %>%
+  na.omit()
 
+taxo <- ucar_d %>% 
+  select(ucar_id, simb, taxo1 = taxo_ppal, taxo2 = taxo_2rio, taxo3 = taxo_3rio) %>% 
+  pivot_longer(cols = c(taxo1, taxo2, taxo3), names_to = 'tipo', values_to = 'taxo') %>% 
+  na.omit()
 
 
 write_csv(hoja, 'data/ucar_x_hoja_ign.csv')
